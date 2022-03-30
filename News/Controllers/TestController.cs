0927ede6d065c797da115c873473874e3d7662d0
DESKTOP-5271TLS\B <thaibao0225@gmail.com>
2022-03-30 13:23:54 +0700

    Add Tiny MCE

---
+++ News/Controllers/TestController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace News.Controllers
+{
+    public class TestController : Controller
+    {
+        [Route("test")]
+        // GET: TestController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: TestController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TestController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TestController/Create
+        [Route("test/create")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                string a = Request.Form["testForm"];
+
+                string s;
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TestController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TestController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TestController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TestController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
