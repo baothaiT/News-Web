60edde5243f1e7e107ef9334053e795fc80bd485
Thaibao25 <baothai0225@gmail.com>
2022-04-13 14:40:30 +0700

    Add author

--- News/Controllers/Staff/CreateIdeaController.cs
+++ News/Controllers/Staff/CreateIdeaController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -43,6 +44,7 @@ namespace News.Controllers.Staff
         }
 
         // GET: CreateIdeaController/Create
+        [Authorize]
         [Route("createiead")]
         [HttpGet("submissionId")]
         public ActionResult Create(string submissionId)
